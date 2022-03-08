057ff4e72d48c18172a69808d874a137595f217d
wafibismail <56105912+wafibismail@users.noreply.github.com>
2022-03-08 10:31:06 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,30 +1,30 @@
 CREATE TABLE PERSON
-(	id			    VARCHAR(32) 	NOT NULL,
+(	id		VARCHAR(32) 	NOT NULL,
 	displayName	VARCHAR(255),
 	email	    	VARCHAR(255),
 	faculty	  	VARCHAR(50),
-	phone		    VARCHAR(15),
+	phone		VARCHAR(15),
 PRIMARY KEY (id)); 
 
 CREATE TABLE MANAGEMENT_ROLE
-(	id		    	VARCHAR(32)		NOT NULL,
-	roleName	  VARCHAR(255)	NOT NULL,
+(	id		VARCHAR(32)		NOT NULL,
+	roleName	VARCHAR(255)		NOT NULL,
 	updateTime	DATETIME	  	NOT NULL,
 PRIMARY KEY (id),
 UNIQUE (roleName));
 
 CREATE TABLE ITEM
-(	id		    	INT			    	NOT NULL,
+(	id		INT		NOT NULL,
 	itemName  	VARCHAR(255)	NOT NULL,
 	categoryId	INT,
-	updateTime	DATETIME	  	NOT NULL,
+	updateTime	DATETIME	NOT NULL,
 PRIMARY KEY (id));
 
 CREATE TABLE CATEGORY
-(	id		    		INT			    	NOT NULL,
-	categoryName	VARCHAR(255)	NOT NULL,
-	parentId	  	INT,
-	updateTime		DATETIME	  	NOT NULL,
+(	id		INT			NOT NULL,
+	categoryName	VARCHAR(255)		NOT NULL,
+	parentId	INT,
+	updateTime	DATETIME		NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (parentId) REFERENCES CATEGORY(id)
 	ON DELETE NO ACTION	  ON UPDATE NO ACTION);
@@ -33,8 +33,8 @@ ALTER TABLE ITEM
   ADD FOREIGN KEY (categoryId) REFERENCES CATEGORY(id) UPDATE CASCADE;
 
 CREATE TABLE OVERLOOKS
-(	roleId		  VARCHAR(32)	 NOT NULL,
-	categoryId	INT			    NOT NULL,
+(	roleId		VARCHAR(32)	NOT NULL,
+	categoryId	INT		NOT NULL,
 PRIMARY KEY (roleId, categoryId),
 FOREIGN KEY (roleId) REFERENCES MANAGEMENT_ROLE(id)
 	ON DELETE CASCADE ON UPDATE CASCADE,
@@ -42,13 +42,13 @@ FOREIGN KEY (categoryId) REFERENCES CATEGORY(id)
 	ON DELETE CASCADE ON UPDATE CASCADE);
 
 CREATE TABLE RESERVATION
-(	id			  		INT	  		NOT NULL,
-	personId			INT		  	NOT NULL,
-	itemId				INT			  NOT NULL,
-	startTime			DATETIME	NOT NULL,
-	endTime				DATETIME	NOT NULL,
-	responderId		VARCHAR(32),
-	respondTime		DATETIME,
+(	id		INT		NOT NULL,
+	personId	INT		NOT NULL,
+	itemId		INT		NOT NULL,
+	startTime	DATETIME	NOT NULL,
+	endTime		DATETIME	NOT NULL,
+	responderId	VARCHAR(32),
+	respondTime	DATETIME,
 	creationTime	DATETIME	NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (itemId) REFERENCES ITEM(id)
