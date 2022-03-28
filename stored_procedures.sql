cda984abc25a9996dc3e3fbce3e2a7516ef8d1a0
wafibismail <56105912+wafibismail@users.noreply.github.com>
2022-03-28 19:50:23 +0800

    Update stored_procedures.sql

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -2,7 +2,7 @@ DROP PROCEDURE IF EXISTS spGetCategories;
 DROP PROCEDURE IF EXISTS spGetConsumables;
 DROP PROCEDURE IF EXISTS spGetNonConsumables;
 
---Need to run the creation sql one by one
+--Creation sql for stored procedures need to be run separately one at a time
 
 CREATE PROCEDURE spGetCategories AS
 BEGIN
@@ -13,11 +13,33 @@ END;
 CREATE PROCEDURE spGetConsumables AS
 BEGIN
 SET NOCOUNT ON
-SELECT * FROM ITEM
+SELECT *,
+  (CASE WHEN EXISTS (
+				SELECT 1
+				FROM RESERVATION R
+			  WHERE ITEM.Id = R.Item_Id
+        AND Start_Time < GETDATE()
+        AND End_Time > GETDATE()
+		  )
+		THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT)
+	END) AS Is_Reserved 
+FROM ITEM, CONSUMABLE_ITEM
+WHERE Id = Item.Id
 END;
 
 CREATE PROCEDURE spGetNonConsumables AS
 BEGIN
 SET NOCOUNT ON
-SELECT * FROM ITEM
+SELECT *,
+  (CASE WHEN EXISTS (
+				SELECT 1
+				FROM RESERVATION R
+			  WHERE ITEM.Id = R.Item_Id
+        AND Start_Time < GETDATE()
+        AND End_Time > GETDATE()
+		  )
+		THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT)
+	END) AS Is_Reserved 
+FROM ITEM, NONCONSUMABLE_ITEM
+WHERE Id = Item.Id
 END;
