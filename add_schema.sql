47503e18c29598c1518f98d5964e12180c11e8ba
wafibismail <56105912+wafibismail@users.noreply.github.com>
2022-03-29 10:50:30 +0800

    Update add_schema.sql

--- add_schema.sql
+++ add_schema.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS PERSON;
 
 CREATE TABLE PERSON --to prevent needless bloating, only record the person's info after he/she has made a booking request
 (	Id		INT		IDENTITY(0,1)	PRIMARY KEY,
-	Graph_Id	VARCHAR(32)	UNIQUE,
+	Graph_Id	VARCHAR(32),
 	Display_Name	VARCHAR(255),
 	Email	    	VARCHAR(255)	NOT NULL,
 	Faculty	  	VARCHAR(50),
@@ -18,7 +18,7 @@ CREATE TABLE CATEGORY
 (	Id		INT		IDENTITY(0,1)	PRIMARY KEY,
 	Category_Name	VARCHAR(50)	NOT NULL UNIQUE,
 	Parent_Id	INT,
-	Update_Time	DATETIME	DEFAULT (GETDATE()),
+	Update_Time	DATETIME	NOT NULL DEFAULT GETDATE(),
 FOREIGN KEY (Parent_Id) REFERENCES CATEGORY(Id)
 	ON DELETE NO ACTION	ON UPDATE NO ACTION);
 
@@ -31,18 +31,18 @@ CREATE TABLE ITEM
  			VARCHAR(255),
 	Category_Id	INT		NOT NULL,
  	Is_Consumable	BIT		NOT NULL,
-	Update_Time	DATETIME	DEFAULT (GETDATE()),
+	Update_Time	DATETIME	NOT NULL DEFAULT GETDATE(),
 FOREIGN KEY (Category_Id) REFERENCES CATEGORY(Id)
  	ON DELETE NO ACTION	ON UPDATE CASCADE);
 
 CREATE TABLE CONSUMABLE_ITEM
 (	Item_Id		INT		NOT NULL,
-	Value_Per_Amt	INT		DEFAULT 0,
-	Value_Unit	VARCHAR(10)	DEFAULT 'unit',
-	Amount_In	INT		DEFAULT 0,
-	Amount_Out	INT		DEFAULT 0,
+	Value_Per_Amt	INT		NOT NULL DEFAULT 0,
+	Value_Unit	VARCHAR(10)	NOT NULL DEFAULT 'pc',
+	Amount_In	INT		NOT NULL DEFAULT 0,
+	Amount_Out	INT		NOT NULL DEFAULT 0,
 	Amount_Bal	AS		(Amount_In - Amount_Out),
-	Amount_Unit	VARCHAR(10)	DEFAULT 'set',
+	Amount_Unit	VARCHAR(10)	NOT NULL DEFAULT 'set',
 	Next_In_Amt	INT,
 	Next_In_Date	DATETIME,
 	Purchase_Date	DATETIME,
@@ -53,14 +53,14 @@ FOREIGN KEY (Item_Id) REFERENCES ITEM(Id)
 CREATE TABLE NONCONSUMABLE_ITEM
 (	Item_Id		INT		NOT NULL,
 	--Is_Reserved calculated in stored procedure
-	Is_Blocked	BIT		DEFAULT 0,
-	Is_OutOfOrder	BIT		DEFAULT 0,
-	Is_Faulty	BIT		DEFAULT 0,
+	Is_Blocked	BIT		NOT NULL DEFAULT 0,
+	Is_OutOfOrder	BIT		NOT NULL DEFAULT 0,
+	Is_Faulty	BIT		NOT NULL DEFAULT 0,
 	Location	VARCHAR(50),
-	Brand		VARCHAR(30)	NOT NULL,
-	Model		VARCHAR(30)	NOT NULL,
-	Equipment_No	VARCHAR(40)	NOT NULL,
-	Serial_No	VARCHAR(40)	NOT NULL,
+	Brand		VARCHAR(30),
+	Model		VARCHAR(30),
+	Equipment_No	VARCHAR(40),
+	Serial_No	VARCHAR(40),
 	Assignee_Email	VARCHAR(255),
 FOREIGN KEY (Item_Id) REFERENCES ITEM(Id)
 	ON DELETE CASCADE	ON UPDATE CASCADE);
@@ -69,19 +69,19 @@ CREATE TABLE RESERVATION
 (	Id		INT		IDENTITY(0,1)	PRIMARY KEY,
 	Item_Id		INT		NOT NULL,
  	Loan_Location	VARCHAR(50),
-	Start_Time	DATETIME	NOT NULL,
- 	Is_Released	BIT		DEFAULT 0,
+	Start_Time	DATETIME	NOT NULL DEFAULT GETDATE(),
+ 	Is_Released	BIT		NOT NULL DEFAULT 0,
 	End_Time	DATETIME	NOT NULL,
- 	Is_Returned	BIT		DEFAULT 0,
+ 	Is_Returned	BIT		NOT NULL DEFAULT 0,
  	Person_Id	INT		NOT NULL,
  	Supervisor_Email	VARCHAR(255)	NOT NULL,
  	Responder_Id_A	INT,
  	Responder_Id_B	INT,
- 	Is_Pending	BIT		DEFAULT 1,
- 	Is_Approved_Sup	BIT		DEFAULT 0,
- 	Is_Approved_A	BIT		DEFAULT 0,
- 	Is_Approved_B	BIT		DEFAULT 0,
-	Creation_Time	DATETIME	DEFAULT (GETDATE()),
+ 	Is_Pending	BIT		NOT NULL DEFAULT 1,
+ 	Is_Approved_Sup	BIT		NOT NULL DEFAULT 0,
+ 	Is_Approved_A	BIT		NOT NULL DEFAULT 0,
+ 	Is_Approved_B	BIT		NOT NULL DEFAULT 0,
+	Creation_Time	DATETIME	NOT NULL DEFAULT (GETDATE()),
 	Update_Time	DATETIME,
 FOREIGN KEY (Item_Id) REFERENCES ITEM(Id)
 	ON UPDATE CASCADE,
